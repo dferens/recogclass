d1ff90cdaa86a597abf9151becf35cfec652a0ed
Ferens Dmitriy <ferensdima@gmail.com>
2014-09-18 22:37:51 +0300

    Implemented 'Spectr' algo

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
   :description "recognition and classification"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [incanter/incanter-charts "1.5.5"]]
   :plugins [[lein-autoreload "0.1.0"]])
